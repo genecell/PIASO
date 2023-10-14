ea0e5d66c736c0d70843c3d89d047d191319ee11
Min Dai <dai@broadinstitute.org>
2023-10-13 23:30:06 -0400

    add new

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -108,7 +108,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Fishell Lab @Harvard Medical School and Broad Institute. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
